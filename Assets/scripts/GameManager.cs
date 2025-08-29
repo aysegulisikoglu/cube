815f843eefaca0b3541a6b01d32b5a44a601e9a4
aysegulisikoglu <aysegulisikoglu08@gmail.com>
2025-08-29 13:03:45 +0300

    a

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject obstacle;
+    public Transform spawnPoint;
+    int score = 0;
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
+    void Start()
+    {
+        GameStart();
+    }
+    void Update()
+    {
+
+    }
+    IEnumerator SpawnObstacles()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(0.8f, 2f);
+            yield return new WaitForSeconds(waitTime);
+            Instantiate(obstacle, spawnPoint.position, Quaternion.identity);
+        }
+    }
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+    public void GameStart()
+    {
+        player.SetActive(true);
+        playButton.SetActive(false);
+
+        StartCoroutine("SpawnObstacles");
+        InvokeRepeating("ScoreUp", 2f, 1f);
+    }
+}
